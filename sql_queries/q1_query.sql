a917e492d2a70108e1eb5e67f705a5769b4259b0
Joel H <himesjoel2107@gmail.com>
2023-07-05 14:37:00 -0400

    new query added

---
+++ sql_queries/q1_query.sql
@@ -0,0 +1,13 @@
+SELECT
+    CONCAT(p.firstname, ' ', p.lastname) AS employee_name,
+    e.jobtitle,
+    ea.emailaddress,
+    e.vacationhours AS accrued_vacation_hours
+FROM
+    employee AS e
+JOIN
+    person.person AS p ON e.businessentityid = p.businessentityid
+JOIN
+    person.emailaddress AS ea ON e.businessentityid = ea.businessentityid
+WHERE
+    e.vacationhours > 40;
